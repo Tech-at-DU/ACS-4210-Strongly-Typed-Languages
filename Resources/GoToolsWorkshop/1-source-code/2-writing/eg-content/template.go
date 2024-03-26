3da8627b4f1371597b901af552a6c9dcebce2a61
Dani Roxberry <dani@bitoriented.com>
2024-03-25 20:08:27 -0400

    [add] new

--- Resources/GoToolsWorkshop/1-source-code/2-writing/eg-content/template.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"errors"
-	"fmt"
-)
-
-func before(s string) error { return fmt.Errorf("%s", s) }
-func after(s string) error  { return errors.New(s) }
--- _navbar.md
+++ _navbar.md
@@ -2,4 +2,3 @@
 
 *  **[Syllabus](README.md)**
 *  **[Project Rubric](Project/MakeUtility.md)**
-* [Make School](https://www.makeschool.com)
